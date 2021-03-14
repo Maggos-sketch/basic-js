2c8fcc98f9f50fe4b7b024368212c35b16c631e0
Max <68688024+Maggos-sketch@users.noreply.github.com>
2021-03-14 13:57:06 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(args) {
+  //throw new CustomError('Not implemented');
+    let isT = 0;
+  let dT = "";
+  for (let arg of args) {
+    if (typeof arg == "string") {
+      dT += arg[0];
+      isT++;
+    }
+  }
+
+  dT = dT.split("").sort().join("");
+  if (isT == 0) {
+    return false;
+  } else {
+    return dT.toUpperCase();
+  }
 };
